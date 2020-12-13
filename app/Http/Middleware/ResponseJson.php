bac5d5c8db88dcc62b5953b38c5bcbb0684435d3
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-13 19:14:42 -0300

    feat(middleware): create middleware to force json response

---
+++ app/Http/Middleware/ResponseJson.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ResponseJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Content-Type', 'application/json');
+
+        $response = $next($request);
+
+        if (!$response instanceof JsonResponse) {
+            $response = $this->factory->json(
+                $response->content(),
+                $response->status(),
+                $response->headers->all()
+            );
+        }
+
+        return $response;
+    }
+}

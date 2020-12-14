af11489dce42704fbab6e520d0a82d6061bf3c5a
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-13 21:15:28 -0300

    feat(routes): create auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Social auth
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('{provider}', [LoginController::class, 'redirectToProvider']);
+    Route::get('{provider}/callback', [LoginController::class, 'handleProviderCallback']);
 });

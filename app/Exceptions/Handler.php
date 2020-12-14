d4abf754f5fb5a97e53ffe6495752c93e89c475e
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-13 22:32:06 -0300

    feat(exception): treat exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -33,8 +39,80 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e, $request) {
+            if ($e instanceof ModelNotFoundException && $request->wantsJson()) {
+                return response()->json([
+                    'message' => trans('message.not_found'),
+                    'error' => trans('message.entry_not_found', ['model' => str_replace('App\\Models\\', '', $e->getModel())])
+                ], 404);
+            }
+
+            if ($e instanceof UnauthorizedHttpException) {
+                $preException = $e->getPrevious();
+                if ($preException instanceof
+                    TokenExpiredException
+                ) {
+                    return response()->json([
+                        'message' => trans('auth.unauthenticated'),
+                        'error' => trans('auth.token_expired')
+                    ], 401);
+                } else if ($preException instanceof
+                    TokenInvalidException
+                ) {
+                    return response()->json([
+                        'message' => trans('auth.unauthenticated'),
+                        'error' => trans('auth.token_invalid')
+                    ], 401);
+                } else if ($preException instanceof
+                    TokenBlacklistedException
+                ) {
+                    return response()->json([
+                        'message' => trans('auth.unauthenticated'),
+                        'error' => trans('auth.token_blacklisted')
+                    ], 401);
+                } else if ($preException instanceof
+                    JWTException
+                ) {
+                    return response()->json([
+                        'message' => trans('auth.unauthenticated'),
+                        'error' => trans('auth.token_cannot_parse')
+                    ], 401);
+                }
+
+                if ($e->getMessage() === 'Token not provided') {
+                    return response()->json([
+                        'message' => trans('auth.unauthenticated'),
+                        'error' => trans('auth.token_not_provided')
+                    ], 401);
+                }
+
+                //To log untreated unauthorized exceptions
+                \Log::error('UNAUTHORIZED_EXCEPTION:' . $e->getMessage());
+                return response()->json([
+                    'message' => trans('auth.unauthenticated'),
+                    'error' => trans('message.contact_support')
+                ], 401);
+            }
+
+            if ($e instanceof
+                JWTException
+            ) {
+                return response()->json([
+                    'message' => trans('auth.unauthenticated'),
+                    'error' => trans('auth.already_logged_out')
+                ], 422);
+            }
+
+            if ($e instanceof
+                UnauthorizedHttpException
+            ) {
+                return response()->json([
+                    'message' => trans('auth.not_found'),
+                    'error' => trans('auth.user_not_found')
+                ], 404);
+            }
+
+            return parent::render($request, $e);
         });
     }
 }

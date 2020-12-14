f64f075d4eb31b6762f3d560e06951c23ae1f182
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-13 21:42:32 -0300

    Feat/vic seed factory (#20)
    * feat(factory): make user factory
    
    * feat(seed): make user seed & add to db

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+            'guard_name' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+            'role' => 1
+        ]);
+
+        if (App::environment('local', 'staging')) {
+            \App\Models\User::factory(10)->create();
+        }
+    }
+}

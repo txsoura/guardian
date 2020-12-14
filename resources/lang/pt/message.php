3255853d56399d30dba946ab6971cf26b9a79616
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-13 22:56:41 -0300

    feat(lang): create pt lang and add dictionary

---
+++ resources/lang/pt/message.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'route_not_found' => 'Rota não encontrada',
+    'entry_not_found' => 'Entrada para :model não encontrado',
+    'not_found' => 'Não encontrado',
+    'deleted' => 'Apagado com sucesso',
+    'no_access' => 'Você não tem acesso ao sistema, entre em contato com o suporte.',
+    'contact_support' => 'Algo errado aconteceu, entre em contato com o suporte.'
+];

a17c1242f5a9d85da31752fc6d2a5ac2e58f74b4
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-19 17:59:32 -0300

    feat(controller): create role controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return RoleResource::collection(
+            Role::when($request['include'], function ($query, $include) {
+                return $query->with(explode(',',  $include));
+            })
+                ->orderBy('created_at', 'desc')
+                ->get(),
+            200
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request['name'] = Str::lower($request['name']);
+
+        $request->validate([
+            'name' => 'required|string|unique:acl_roles',
+            'description' => 'required|string'
+        ]);
+
+
+        $role = Role::create($request->all());
+
+        return new RoleResource($role, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Role $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, Role $role)
+    {
+        return new RoleResource(
+            Role::find($role->id)
+                ->when($request['include'], function ($query, $include) {
+                    return $query->with(explode(',',  $include));
+                })
+                ->firstOrFail(),
+            200
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Role $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $request['name'] = Str::lower($request['name']);
+
+        $request->validate([
+            'name' => 'string|unique:acl_roles',
+            'description' => 'string'
+        ]);
+
+        $role->update($request->all());
+
+        return new RoleResource($role, 202);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Role $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return response()->json(['message' => trans('message.deleted')], 204);
+    }
+}

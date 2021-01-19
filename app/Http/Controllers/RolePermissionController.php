5c18d2b43f01cd19feefdbed68f1027a29ed72b7
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-19 17:59:50 -0300

    feat(controller):  create rolePermission controller

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RolePermissionResource;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\RolePermission;
+use Illuminate\Http\Request;
+
+class RolePermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, Role $role)
+    {
+        return RolePermissionResource::collection(
+            RolePermission::where('acl_role_id', $role->id)->get(),
+            200
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Role $role)
+    {
+        $request['acl_role_id'] = $role->id;
+
+        $request->validate([
+            'acl_role_id' => 'required|numeric||exists:acl_roles,id',
+            'acl_permission_id' => 'required|numeric|exists:acl_permissions,id'
+        ]);
+
+        $rolePermission = RolePermission::create($request->all());
+
+        return new RolePermissionResource($rolePermission, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Role $role
+     * @param  Permission $permission
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role, Permission $permission)
+    {
+        RolePermission::where('acl_role_id', $role->id)
+            ->where('acl_permission_id', $permission->id)
+            ->delete();
+
+        return response()->json(['message' => trans('message.deleted')], 204);
+    }
+}

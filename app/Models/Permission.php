6d1baf369da85d1fa84f7b370f98c42b33f5974b
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-19 01:39:21 -0300

    feat(model): create permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Permission extends Model
+{
+    use Notifiable, SoftDeletes;
+
+    protected $table = 'acl_permissions';
+    protected $primaryKey = 'id';
+    protected $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'model', 'description'
+    ];
+}

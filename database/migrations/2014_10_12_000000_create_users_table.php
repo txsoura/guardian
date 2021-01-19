b222f039f3887b037ae454337b99da97f4c77a4f
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-19 18:02:56 -0300

    feat(migrations): remove profile table from migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,24 +17,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->enum('status', UserStatus::toArray())->default(UserStatus::PENDENT);
             $table->foreignId('role_id')->references('id')->on('acl_roles');
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
-        Schema::create('profiles', function (Blueprint $table) {
-            $table->foreignId('user_id')->references('id')->on('users');
-            $table->primary('user_id');
-            $table->string('img')->nullable();
-            $table->enum('sex', Sex::toArray())->nullable();
-            $table->date('birthdate')->nullable();
-            $table->bigInteger('cellphone')->nullable();
-            $table->string('name')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -47,7 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profiles');
         Schema::dropIfExists('users');
     }
 }

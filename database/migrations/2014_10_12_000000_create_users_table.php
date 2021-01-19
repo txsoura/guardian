cf9d8a73eb0eb451d7ece968f756e50509688b64
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-18 23:53:02 -0300

    feat(migrations): improve user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Sex;
 use App\Enums\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,13 +17,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->enum('status', UserStatus::toArray())->default(UserStatus::PENDENT);
-            $table->foreignId('role')->references('id')->on('roles');
+            $table->foreignId('role_id')->references('id')->on('acl_roles');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->primary('user_id');
+            $table->string('img')->nullable();
+            $table->enum('sex', Sex::toArray())->nullable();
+            $table->date('birthdate')->nullable();
+            $table->bigInteger('cellphone')->nullable();
+            $table->string('name')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -35,6 +47,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('profiles');
         Schema::dropIfExists('users');
     }
 }

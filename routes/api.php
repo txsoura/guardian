eb1cce29f0326110d77fbc18527cb3f87fedb34b
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-20 10:23:36 -0300

    feat(routes): remove unused api routes imports

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\ConfirmPasswordController;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\ResetPasswordController;
-use App\Http\Controllers\Auth\VerificationController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*

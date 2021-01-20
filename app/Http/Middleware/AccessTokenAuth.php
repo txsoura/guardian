cfd9c3db4f0365ac529450fe198e1fcb615ceb30
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-20 11:26:18 -0300

    feat(middleware): create accessToken auth check

---
+++ app/Http/Middleware/AccessTokenAuth.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AccessToken;
+use Closure;
+
+class AccessTokenAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('Authorization');
+
+        $accessToken = AccessToken::where('token', $token)->first();
+
+        if (!$accessToken) {
+            return response()->json([
+                'message' => trans('auth.unauthenticated'),
+                'error' => trans('auth.invalid_token')
+            ], 401);
+        }
+
+        if ($accessToken->expiration < today()) {
+            return response()->json([
+                'message' => trans('auth.unauthenticated'),
+                'error' => trans('auth.token_expired')
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}

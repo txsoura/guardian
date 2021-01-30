7149d40b82709225cb9b6c7ef99317a39b6c3a01
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-30 15:45:00 -0300

    feat(resource): add user name

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => $this->name,
             'email' => $this->email,
             'status' => $this->status,
             'role' => $this->role->name,

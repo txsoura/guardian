55fcbb2c0ebb6d9ab7ce368abc72e709f742fb4d
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-30 16:31:26 -0300

    feat(lang): create es message dictionary words

---
+++ resources/lang/es/message.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'route_not_found' => 'Ruta no encontrada',
+    'app_not_found' => 'App no encontrada',
+    'entry_not_found' => 'Entrada de :model no encontrada',
+    'not_found' => 'No encontrado',
+    'deleted' => 'Borrado exitosamente',
+    'permission_not_found' => 'Permiso no encontrado',
+    'no_permission' => 'No tienes permiso para realizar esta operación.',
+    'no_access' => 'No tiene acceso al sistema, comuníquese con el soporte.',
+    'role_not_found' => 'Papel no encontrado',
+];

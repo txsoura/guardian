e3ca46eb30ef166ab639649ff07e8c8707e652c8
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-30 21:38:02 -0300

    fix(lang): change pt message deleted word

--- resources/lang/pt/message.php
+++ resources/lang/pt/message.php
@@ -5,7 +5,7 @@ return [
     'app_not_found' => 'App não encontrado',
     'entry_not_found' => 'Entrada para :model não encontrado',
     'not_found' => 'Não encontrado',
-    'deleted' => 'Deleted successfully',
+    'deleted' => 'Apagado com successo',
     'permission_not_found' => 'Permissão não encontrada',
     'no_permission' => 'Você não tem permissão para realizar esta operação',
     'no_access' => 'Você não tem acesso ao sistema, entre em contato com o suporte.',

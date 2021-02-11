f981933e845c5caa6011141853153ee836fd89a1
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-02-10 21:55:34 -0300

    feat(seed): rename admin user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
         ]);
 
         User::create([
-            'name' => 'TxsSec Guardian Admin User',
+            'name' => 'Txsoura Guardian Admin User',
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),

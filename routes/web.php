485595cd509162d00b0504dd39fb8df90f182913
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-02-16 17:20:49 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('{provider}', 'Auth\LoginController@redirectToProvider');
+});

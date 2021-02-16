9d79d9a2f1cbd2d7c4bcfce301cfee358e315932
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-02-16 15:35:01 -0300

    feat(migrations): add cellphone & two_factor columns in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enums\Sex;
+use App\Enums\TwoFactorProvider;
 use App\Enums\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -20,10 +21,13 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('cellphone')->unique()->nullable();
+            $table->timestamp('cellphone_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->enum('status', UserStatus::toArray())->default(UserStatus::PENDENT);
             $table->foreignId('role_id')->references('id')->on('acl_roles');
+            $table->enum('two_factor_provider', TwoFactorProvider::toArray())->nullable();
             $table->timestamps();
             $table->softDeletes();
         });

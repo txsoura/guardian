52429d5546754504a12f54aaf89ef23a4b7daed2
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-20 22:29:31 -0300

    refactor(resource): improve user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -17,16 +18,30 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'avatar' => $this->avatar(),
             'email' => $this->email,
             'cellphone' => $this->cellphone,
             'status' => $this->status,
-            'role' => $this->role->name,
+            'role' => $this->role ? $this->role->name : '',
             'email_verified_at' => $this->email_verified_at,
             'cellphone_verified_at' => $this->cellphone_verified_at,
+            'two_factor_enabled' => $this->two_factor_provider ? true : false,
             'permissions' => RolePermissionResource::collection($this->whenLoaded('permissions')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'deleted_at' => $this->deleted_at
         ];
+    }
+
+    private function avatar()
+    {
+        if (!empty($this->avatar)) {
+            //Check if avatar is from provider
+            if (str_contains($this->avatar, 'https')) {
+                return $this->avatar;
+            }
+
+            return config('app.url') . Storage::url($this->avatar);
+        }
     }
 }

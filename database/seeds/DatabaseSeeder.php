af38a3ad77979c769b5938f4252d5723c9e321d2
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-20 23:47:45 -0300

    Refactor/vic seeds (#106)
    * feat(seed): create admin user seed
    
    * feat(seed): create role permission seed
    
    * refactor(seed): improve user seed
    
    * feat(seed): add new seeders to seed
    
    * 0.0.4.1: app version

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        $this->call([
+            RolePermissionSeeder::class,
+            AdminUserSeeder::class
+        ]);
+
+
+
+        if (App::environment('local')) {
+            $this->call(UserSeeder::class);
+        }
     }
 }

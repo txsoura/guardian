6a2635c692b8ac757b96d2019190dfd767f36c34
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-20 23:54:51 -0300

    feat(lang): update cellphone en dictionary

---
+++ resources/lang/en/cellphone.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'sent' => 'Cellphone verification SMS sent',
+    'already_verified' => 'Cellphone already verified',
+    'verified' => 'Cellphone verified',
+    'send' => [
+        'message' => 'Cannot send cellphone verification SMS',
+        'error' => 'Invalid cellphone number',
+    ],
+    'verify' => [
+        'message' => 'Cannot verify cellphone',
+        'error' => 'Invalid verification code',
+    ]
+];

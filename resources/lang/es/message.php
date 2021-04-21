948e3d72450f171af83598f6dc59f12ec33ba575
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-20 23:57:35 -0300

    feat(lang): update message es dictionary

--- resources/lang/es/message.php
+++ resources/lang/es/message.php
@@ -4,10 +4,12 @@ return [
     'route_not_found' => 'Ruta no encontrada',
     'app_not_found' => 'App no encontrada',
     'entry_not_found' => 'Entrada de :model no encontrada',
+    'relation_not_found' => 'Relación no encontrada',
     'not_found' => 'No encontrado',
     'deleted' => 'Borrado exitosamente',
     'permission_not_found' => 'Permiso no encontrado',
     'no_permission' => 'No tienes permiso para realizar esta operación.',
     'no_access' => 'No tiene acceso al sistema, comuníquese con el soporte.',
     'role_not_found' => 'Papel no encontrado',
+    'invalid_signature' => 'Firma inválida',
 ];

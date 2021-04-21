44d8810032f28270443b70968749f1a7a46f1e41
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-20 23:58:33 -0300

    feat(lang): update two factor es dictionary

---
+++ resources/lang/es/twoFactor.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'enable' => [
+        'message' => 'No se puede habilitar la autenticación de dos factores',
+        'mail' => [
+            'error' => 'Verifique su correo electrónico para habilitar la autenticación de dos factores',
+        ],
+        'sms' => [
+            'error' => 'Verifique su teléfono celular para habilitar la autenticación de dos factores',
+        ],
+    ],
+    'recovery' => [
+        'message' => 'No se puede recuperar la autenticación de dos factores',
+        'error' => 'Código de recuperación no válido',
+    ],
+    'verify' => [
+        'message' => 'No se puede verificar la autenticación de dos factores',
+        'error' => 'código de verificación invalido',
+    ],
+    'verified' => 'Autenticación de dos factores verificada'
+];

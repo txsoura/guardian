05863890fe64bb05e8715d9ea56ee0270fa996ca
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-21 00:12:13 -0300

    config(config): add app view_url

--- config/app.php
+++ config/app.php
@@ -67,6 +67,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'view_url' => env('APP_VIEW_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
